f7072e7511f4fe74ac156e699701e26f93e20e8f
xindl0423 <147639977+xindl0423@users.noreply.github.com>
2025-04-08 16:03:38 -0400

    just coppied tutorial code lol

--- include/seg7.h
+++ include/seg7.h
@@ -5,5 +5,5 @@
 
 extern uint8_t digits[ARRAY_SIZE];
 
-void segment_init();
 void displyValue(byte value);
+
--- src/seg7.cpp
+++ src/seg7.cpp
@@ -1,28 +1,13 @@
-#include <seg7.h>
-#include <PINinit.h>
-#include <util/delay.h>
+#include"Segment.h"
+#include"ShiftReg.h"
+#include <PINint.h>
 
-/*
-uint8_t digits[ARRAY_SIZE] = {0xEE, 0x82, 0xDC, 0xD6, 0xB2, 0x76, 0x7E, 0xC2, 0xFE, 0xF6, 0xFA, 0xFE, 0x6C, 0xEE, 0x7C, 0x78};
-
-void segment_init() {
-    DDRD = 0xFF;    // PORTD (0-7) as outputs for segments
-    DDRB |= 0x0F;   // PB0-PB3 (8-11) for digit selection
-    DDRC |= 0x1E;   // PC1-PC4 (A1-A4) for digit selection
-    
-    // Initialize button
-    DDRC &= ~(1<<Button);
-    PORTC |= (1<<Button);
-    
-    // Initialize switches
-    DDRB &= ~((1<<switch0) | (1<<switch1));
-    PORTB |= ((1<<switch0) | (1<<switch1));
+void displyValue(byte value)
+{
+    //set latch to low
+      PORTD&=~(1<<LATCH);
+      //shift the data in shift register
+      myShiftOut(DATA, CLOCK, LSBFIRST, value);
+      //set the latch to high again
+      PORTD|=(1<<LATCH);
 }
-
-void displaydigits() {
-    static uint8_t current_digit = 0;
-    PORTD = digits[current_digit];
-    current_digit = (current_digit + 1) % ARRAY_SIZE;
-    _delay_ms(500);
-}
-    */
